0af8e0c5f799590f663606cbd2029cb3d31d1fa2
Amr <amrhossam.dev@gmail.com>
2025-05-05 21:46:32 +0300

    Add region bounds calculation to agency coverage api
    - Resolved an issue where an empty agency was returned.

--- internal/gtfs/gtfs_manager.go
+++ internal/gtfs/gtfs_manager.go
@@ -83,6 +83,44 @@ func (manager *Manager) GetAgencies() []gtfs.Agency {
 	return manager.gtfsData.Agencies
 }
 
+func (manager *Manager) GetRegionBounds() (lat, lon, latSpan, lonSpan float64) {
+	var minLat, maxLat, minLon, maxLon float64
+	first := true
+	fmt.Println("Shapes: ", len(manager.gtfsData.Shapes))
+	for _, shape := range manager.gtfsData.Shapes {
+		for _, point := range shape.Points {
+			if first {
+				minLat = point.Latitude
+				maxLat = point.Latitude
+				minLon = point.Longitude
+				maxLon = point.Longitude
+				first = false
+				continue
+			}
+
+			if point.Latitude < minLat {
+				minLat = point.Latitude
+			}
+			if point.Latitude > maxLat {
+				maxLat = point.Latitude
+			}
+			if point.Longitude < minLon {
+				minLon = point.Longitude
+			}
+			if point.Longitude > maxLon {
+				maxLon = point.Longitude
+			}
+		}
+	}
+
+	lat = (minLat + maxLat) / 2
+	lon = (minLon + maxLon) / 2
+	latSpan = maxLat - minLat
+	lonSpan = maxLon - minLon
+
+	return lat, lon, latSpan, lonSpan
+}
+
 func (manager *Manager) PrintStatistics() {
 	fmt.Printf("URL: %s\n", manager.gtfsURL)
 	fmt.Printf("Last Updated: %s\n", manager.lastUpdated)
